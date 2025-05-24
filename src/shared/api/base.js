cfab74345a797e2120e1e4c5955d3db7ef06f011
DeadMonstr <deedmonstr@gmail.com>
2025-05-24 15:21:19 +0500

    added new features

--- src/shared/api/base.js
+++ src/shared/api/base.js
@@ -1,14 +1,12 @@
 //
 //
 //
-export const API_URL_DOC = `http://26.253.30.50:8000`
-// export const API_URL_DOC = `https://api.xususiytalim.uz`
-
+// export const API_URL_DOC = `http://26.253.30.50:8000`
+export const API_URL_DOC = `https://api.xususiytalim.uz`
 export const API_URL_IMG= `https://api.xususiytalim.uz`
 // export const API_URL_DOC = `http://26.253.30.50:8000`
 export const API_URL = `${API_URL_DOC}/api/`
 
-//
 // export const API_URL_DOC = `https://ntt.avotra.ru/`
 // export const API_URL = `${API_URL_DOC}api/`
 
